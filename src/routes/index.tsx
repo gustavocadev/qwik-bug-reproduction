42595cd9cb4fd623a71b601e4a698b1d005e56b8
gustavocadev <gustavocadev@gmail.com>
2023-08-04 16:11:54 -0500

    Add changes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,40 @@
-import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import { component$ } from '@builder.io/qwik';
+import { Form, type DocumentHead } from '@builder.io/qwik-city';
 
 export default component$(() => {
   return (
     <>
-      <h1>Hi 👋</h1>
-      <p>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </p>
+      <Form
+        onSubmit$={() => {
+          // BUG: this is never executed, in other frameworks this should be working
+          console.log('Form submitted using method GET');
+        }}
+        class="flex flex-col space-y-2 container mx-auto py-12"
+      >
+        <label for="">Name</label>
+        <input
+          type="text"
+          name="name"
+          class="border p-2"
+          placeholder="Write your name"
+        />
+        <button
+          type="submit"
+          class="p-2 w-full bg-slate-700 hover:bg-slate-800 text-white transition-colors"
+        >
+          Submit
+        </button>
+      </Form>
     </>
   );
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: 'Welcome to Qwik',
   meta: [
     {
-      name: "description",
-      content: "Qwik site description",
+      name: 'description',
+      content: 'Qwik site description',
     },
   ],
 };
